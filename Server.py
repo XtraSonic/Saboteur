01293e7ec69150f13b689458f8b593a813c3f07f
XtraSonic <stanciu.sonic@gmail.com>
2018-01-13 13:52:10 +0200

    Server sends player objects

--- Server.py
+++ Server.py
@@ -1,7 +1,8 @@
 import socket
 import SaboteurModel as sm
 import threading
-
+import pickle
+import os
 
 class Server:
     PORT = 4315
@@ -14,52 +15,98 @@ class Server:
         print(socket.gethostname())
         self.server_socket.bind((socket.gethostname(), Server.PORT))
         self.server_socket.listen(10)
-        self.game_started = False
         self.nr_of_players = nr_of_players
-        self.start()
+        self.client_sockets = [None for _ in range(self.nr_of_players)]
+        """:type client_sockets: list[socket.SocketType]"""
+        self.wait_for_players()
+
         while None in self.player_names:
             self.player_names.remove(None)
 
         self.model = sm.Model(self.player_names)
-        # todo
-        print("ALL GOOD YOU BASTARTD ;)")
+        print("Model Created")
+
+    def start_game(self):
+        print("Starting game")
+        for index in range(len(self.client_sockets)):
+            client_socket = self.client_sockets[index]
+            # server is ready
+            client_socket.send("START".encode())
+
+            # send player object
+            player_file = open("Player" + str(index), 'wb')
+            pickle.dump(self.model.players[index], player_file)
+            player_file.close()
+            player_file = open("Player" + str(index), "rb")
+            client_socket.sendfile(player_file)
+            player_file.close()
+
+            # cleanup
+            os.remove("Player" + str(index))
+
+        print("Done Sending")
+        while threading.active_count() - 1:
+            pass
+
+    def end_listening(self):
         self.server_socket.close()
 
+    def end_game(self):
+        for client_socket in self.client_sockets:
+            client_socket.send("END".encode())
+            client_socket.close()
 
-    def start(self):
-        while not self.game_started:
+    def wait_for_players(self):
+        enough_players = False
+        while not enough_players:
             # accept connections from outside
             (client_socket, address) = self.server_socket.accept()
             name = client_socket.recv(64).decode()
             if name == 0:
                 continue
             client_id = self.get_first_available_index(name)
+            with self.lock:
+                self.player_names[client_id] = name
+                self.client_sockets[client_id] = client_socket
             print(name, "connected with id", client_id)
-            t = threading.Thread(target=self.client_listener, args=(client_socket, client_id))
+            t = threading.Thread(target=self.client_handler, args=(client_socket, client_id))
             t.daemon = True
             t.start()
 
             if threading.active_count() - 1 == self.nr_of_players:
-                self.game_started = True
+                enough_players = True
 
     def get_first_available_index(self, name):
         i = 0
         with self.lock:
             while self.player_names[i] is not None:
                 i += 1
-            self.player_names[i] = name
-
         return i
 
-    def client_listener(self, com_line, client_id):
+    def client_handler(self, com_line, client_id):
         assert isinstance(com_line, socket.SocketType)
-        ready = com_line.recv(1)
-        if ready == 0:
-            with self.lock:
-                self.player_names[client_id] = None
+        try:
+            # wait for the client to be set up
+            ready = com_line.recv(1)
+            if ready == 0:
+                self.remove_client_name(client_id)
+                com_line.close()
+                return
+
+        except socket.error:
+            self.remove_client_name(client_id)
+            com_line.close()
             return
 
         print("GAME STARTED FOR ", client_id)
 
-# s = Server(3)
-# s.start()
+    def remove_client_name(self, client_id):
+            print("Connection lost for", client_id)
+            with self.lock:
+                self.player_names[client_id] = None
+            return
+
+
+s = Server(3)
+# s.end_listening()
+s.start_game()
